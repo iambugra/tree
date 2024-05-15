6d8cf0e8e8c876511579c5367783904c541d6d84
Bugra Alparslan <iambugraalparslan@gmail.com>
2024-05-15 21:31:37 +0300

    remove dead code

--- main.go
+++ main.go
@@ -58,17 +58,11 @@ func tree(basePath string, indent string) {
 		}
 
 		lenIndent := len(indent) / 3 // Every kind of line in boxLines is 3 bytes long.
-		//fmt.Printf("%d len horizontal line \n", len(boxLines.horizontal))
-		//fmt.Printf("%d len indent\n", lenIndent)
-		//fmt.Printf("Provided indent is |%s| for file %s\n", indent, fileName)
 		if lenIndent < 3 {
-			//fmt.Printf("len short if\n")
 			modifiedIndent = indent
 		} else if string([]rune(indent)[lenIndent-3]) == boxLines.upAndRight {
-			//fmt.Printf("Upright if\n")
 			modifiedIndent = indent[:lenIndent-3] + "   "
 		} else if string([]rune(indent)[lenIndent-3]) == boxLines.verticalAndRight {
-			//fmt.Printf("verticalAndRight if\n")
 			modifiedIndent = indent[:lenIndent-3] + boxLines.vertical + "  "
 		}
 
@@ -92,14 +86,6 @@ func printLine(fileInfo os.FileInfo, indent string) {
 		fmt.Printf("%s%s%s\n", color, indent, fileInfo.Name())
 	} else if !strings.HasPrefix(fileInfo.Name(), ".") {
 		fmt.Printf("%s%s%s\n", color, indent, fileInfo.Name())
-	}
-}
-
-func formattedPrint(fileName string, indent string, color string) {
-	if *hiddenFlag {
-		fmt.Printf("%s%s%s\n", color, indent, fileName)
-	} else if !strings.HasPrefix(fileName, ".") {
-		fmt.Printf("%s%s%s\n", color, indent, fileName)
 	}
 }
 
