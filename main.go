d0697ea3e0aa344374a19345a8f93aad3d1c4906
Bugra Alparslan <iambugraalparslan@gmail.com>
2024-05-15 21:28:59 +0300

    print tree with a bug (2nd indent level is printed wrong)

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ type BoxDrawings struct {
 	verticalAndRight string
 }
 
+var boxLines BoxDrawings
+
 const colorCyanBold = "\033[1;36m"
 const colorNone = "\033[0m"
 
@@ -22,8 +24,6 @@ var dirCount int32
 var fileCount int32
 var hiddenFlag *bool
 
-// only path could work
-func tree(basePath string, indent int) {
 func tree(basePath string, indent string) {
 	f, err := os.Open(basePath)
 	if err != nil {
@@ -31,26 +31,68 @@ func tree(basePath string, indent string) {
 	}
 	defer f.Close()
 
-	fInfo, err := f.Readdir(-1)
+	fileInfo, err := f.Stat()
+	printLine(fileInfo, indent)
+	if !fileInfo.IsDir() {
+		return
+	}
+
+	fileInfos, err := f.Readdir(-1)
 	if err != nil {
 		fmt.Println("Error occurred while reading directory:", err)
 		os.Exit(1)
 	}
 
-	for _, file := range fInfo {
+	lenFiles := len(fileInfos)
+	twoHorizontalLines := boxLines.horizontal + boxLines.horizontal
+	var connectingLine string
+	var modifiedIndent string
+
+	for i, file := range fileInfos {
 		fileName := file.Name()
 
-		if file.IsDir() {
-			dirCount += 1
-			formattedPrint(fileName, indent, colorCyanBold)
-			tree(path.Join(basePath, fileName), indent+"    ")
+		if i == lenFiles-1 {
+			connectingLine = boxLines.upAndRight
 		} else {
-			fileCount += 1
-			formattedPrint(fileName, indent, colorNone)
+			connectingLine = boxLines.verticalAndRight
 		}
+
+		lenIndent := len(indent) / 3 // Every kind of line in boxLines is 3 bytes long.
+		//fmt.Printf("%d len horizontal line \n", len(boxLines.horizontal))
+		//fmt.Printf("%d len indent\n", lenIndent)
+		//fmt.Printf("Provided indent is |%s| for file %s\n", indent, fileName)
+		if lenIndent < 3 {
+			//fmt.Printf("len short if\n")
+			modifiedIndent = indent
+		} else if string([]rune(indent)[lenIndent-3]) == boxLines.upAndRight {
+			//fmt.Printf("Upright if\n")
+			modifiedIndent = indent[:lenIndent-3] + "   "
+		} else if string([]rune(indent)[lenIndent-3]) == boxLines.verticalAndRight {
+			//fmt.Printf("verticalAndRight if\n")
+			modifiedIndent = indent[:lenIndent-3] + boxLines.vertical + "  "
+		}
+
+		tree(path.Join(basePath, fileName), modifiedIndent+connectingLine+twoHorizontalLines)
 	}
 
 	return
+}
+
+func printLine(fileInfo os.FileInfo, indent string) {
+	var color string
+	if fileInfo.IsDir() {
+		dirCount += 1
+		color = colorCyanBold
+	} else {
+		fileCount += 1
+		color = colorNone
+	}
+
+	if *hiddenFlag {
+		fmt.Printf("%s%s%s\n", color, indent, fileInfo.Name())
+	} else if !strings.HasPrefix(fileInfo.Name(), ".") {
+		fmt.Printf("%s%s%s\n", color, indent, fileInfo.Name())
+	}
 }
 
 func formattedPrint(fileName string, indent string, color string) {
@@ -72,6 +114,9 @@ func main() {
 	hiddenFlag = flag.Bool("a", false, "All files are listed.")
 	flag.Parse()
 
+	// set BoxDrawings struct
+	setBoxLines(&boxLines)
+
 	if len(flag.Args()) == 0 {
 		fmt.Println("No arguments given. Give exactly one directory.")
 		os.Exit(1)
@@ -82,8 +127,7 @@ func main() {
 	}
 	basePath := flag.Args()[0]
 
-	fmt.Printf("%s%s\n", colorCyanBold, basePath)
-	tree(basePath, "    ")
+	tree(basePath, "")
 
 	fmt.Printf("\n%d directories, %d files\n", dirCount, fileCount)
 }
